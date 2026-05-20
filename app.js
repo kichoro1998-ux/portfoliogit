0040922e6e40fdc018ce2a6959a284c76996ad0e
momsed <starmomsed@gmail.com>
2026-05-20 16:47:31 +0300

    Improve dark mode toggle text

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const darkBtn = document.getElementById("darkBtn");
 
 darkBtn.addEventListener("click", () => {
-
     document.body.classList.toggle("dark-mode");
 
+    if (document.body.classList.contains("dark-mode")) {
+        darkBtn.textContent = "Light Mode";
+    } else {
+        darkBtn.textContent = "Dark Mode";
+    }
 });
-
